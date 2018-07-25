679b460105d05c376c86c28814a0dbc7cdd5e041
Mark Finger <markfinger@gmail.com>
2018-07-26 09:04:19 +1000

    setup.py fixes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from webpack_manifest import webpack_manifest
 setup(
     name='webpack-manifest',
     version=webpack_manifest.__version__,
-    packages=['webpack_manifest'],
+    packages=['webpack_manifest', 'webpack_manifest.templatetags'],
     description='Manifest loader that allows you to include references to files built by webpack',
     long_description='Documentation at https://github.com/markfinger/python-webpack-manifest',
     author='Mark Finger',
